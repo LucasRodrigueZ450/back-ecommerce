ac3867032a904daa66a47dfd540eb76704854256
Lucas Rodrigues <65141835+LucasRodrigueZ450@users.noreply.github.com>
2025-11-29 19:09:20 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { PORT } from "./config/env.js";
 import { connectDatabase } from "./config/database.js";
-
+const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await connectDatabase();
